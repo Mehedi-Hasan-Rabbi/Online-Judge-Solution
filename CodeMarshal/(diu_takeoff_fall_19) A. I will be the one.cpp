c5f331a03a2853cbbb62a58669c59624254cc84e
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2023-07-02 10:32:05 +0600

    Solved

---
+++ CodeMarshal/(diu_takeoff_fall_19) A. I will be the one.cpp
@@ -0,0 +1,9 @@
+// https://algo.codemarshal.org/contests/diu_takeoff_fall_19/problems/A
+#include<iostream>
+using namespace  std;
+
+int main()
+{
+    cout << "DJ NAJ, I will be the one" << endl;
+    return 0;
+} 
---
+++ CodeMarshal/(icpc-dhaka-20) K. Fare Thee Well My Friends.cpp
@@ -0,0 +1,10 @@
+// https://algo.codemarshal.org/contests/icpc-dhaka-20/problems/K
+// https://drive.google.com/file/d/18jGDn3YRcgJ61tUFruMvMKqGiHMw1xTY/view
+#include<iostream>
+using namespace  std;
+
+int main()
+{
+    cout << 26 << endl;
+    return 0;
+} 
