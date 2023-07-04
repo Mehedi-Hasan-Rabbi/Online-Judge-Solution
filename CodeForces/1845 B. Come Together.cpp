c07171225270d119bfcdd35ac43da3114e4bf674
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2023-07-05 00:14:08 +0600

    1845 B Solved

---
+++ CodeForces/1845 B. Come Together.cpp
@@ -0,0 +1,66 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+#define endl               '\n'
+#define yes                cout << "YES" << endl
+#define no                 cout << "NO" << endl
+#define show(x)            cout << #x << " : " << x << endl
+#define all(a)             a.begin(),a.end()
+#define pb                 push_back
+#define pf                 push_front
+#define mp                 make_pair
+#define ff                 first
+#define ss                 second
+#define input_arr(vec)     for(auto &&it:vec) {cin >> it;}
+#define display_arr(vec)   for (auto &&it : vec){cout << it << " ";} cout << endl;
+#define files              freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define efficient          ios_base::sync_with_stdio(0); cin.tie(0);
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+ll calc (ll x, ll y, ll z) {
+    if (y >= x and z >= x)
+    return min(y-x, z-x);
+
+    if (y <= x and z <= x)
+    return min(x-y, x-z);
+
+    return 0;
+}
+
+void solve()
+{
+    ll xa, ya; cin >> xa >> ya;
+    ll xb, yb; cin >> xb >> yb;
+    ll xc, yc; cin >> xc >> yc;
+
+    ll horizontal = calc(xa, xb, xc);
+    ll virtical = calc(ya, yb, yc);
+    ll ans = horizontal + virtical + 1;
+
+    cout << ans << endl;
+ }
+
+int main(void)
+{
+    //efficient;
+
+    int t = 1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
