b2dc9a42c6b98d1918297f9ae2ca7bcb9da174c4
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2023-07-07 12:13:26 +0600

    1418A and 1614A Solved

---
+++ CodeForces/1418 A. Buying Torches.cpp
@@ -0,0 +1,56 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+#define endl               '\n'
+#define yes                cout << "YES" << endl
+#define no                 cout << "NO" << endl
+#define show(x)            cout << #x << " : " << x << endl
+#define all(a)             a.begin(),a.end()
+#define pb                 push_back
+#define pf                 push_front
+#define mp                 make_pair
+#define ff                 first
+#define ss                 second
+#define display_arr(vec)   for (auto &&it : vec){cout << it << " ";} cout << endl;
+#define files              freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define efficient          ios_base::sync_with_stdio(0); cin.tie(0);
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    ll x, y, k; cin >> x >> y >> k;
+    ll tot_need = (y * k) + k;
+    x = x - 1;
+    ll ans = (tot_need - 1) / x;
+
+    if ((ans*x)+1 != tot_need) ans++;
+
+    ans = ans + k; 
+    
+    cout << ans << endl;
+}
+
+int main(void)
+{
+    //efficient;
+
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
