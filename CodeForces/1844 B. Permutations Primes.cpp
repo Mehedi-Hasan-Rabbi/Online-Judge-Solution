602500054c1438b62909909996a1c9babcce7a41
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2023-07-12 00:08:44 +0600

    Create 1844 B. Permutations Primes.cpp

---
+++ CodeForces/1844 B. Permutations Primes.cpp
@@ -0,0 +1,68 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+#define endl               '\n'
+#define yes                cout << "YES" << endl
+#define no                 cout << "NO" << endl
+#define show(x)            cout << #x << " : " << x << endl
+#define all(a)             a.begin(),a.end()
+#define pb                 push_back
+#define pf                 push_front
+#define mp                 make_pair
+#define ff                 first
+#define ss                 second
+#define input_arr(vec)     for(auto &&it:vec) {cin >> it;}
+#define display_arr(vec)   for (auto &&it : vec){cout << it << " ";} cout << endl;
+#define files              freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define efficient          ios_base::sync_with_stdio(0); cin.tie(0);
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    int n; cin >> n;
+    vi per(n, 0);
+
+    if (n == 1) {cout << 1 << endl; return;}
+    if (n == 2) {cout << 2 << " " << 1 << endl; return;}
+
+    per[0] = 2;
+    per[n/2] = 1;
+    per[n-1] = 3;
+
+    int c = 4;
+    
+    for (int i = 0; i < n; i++)
+    {
+        if (per[i] == 0) {
+            per[i] = c;
+            c++;
+        }
+    }
+    
+    display_arr(per);
+}
+
+int main(void)
+{
+    efficient;
+
+    int t = 1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
