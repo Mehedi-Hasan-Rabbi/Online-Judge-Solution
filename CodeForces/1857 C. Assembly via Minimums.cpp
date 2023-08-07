b57b137a1378ab23b4e7734f7ea02dd9284b61e8
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2023-08-07 23:26:22 +0600

    1609C Solved 7/08/2023

---
+++ CodeForces/1857 C. Assembly via Minimums.cpp
@@ -0,0 +1,67 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+#define endl               '\n'
+#define yes                cout << "YES" << endl
+#define no                 cout << "NO" << endl
+#define show(x)            cout << #x << " : " << x << endl
+#define all(a)             a.begin(),a.end()
+#define pb                 push_back
+#define pf                 push_front
+#define mp                 make_pair
+#define ff                 first
+#define ss                 second
+#define input_arr(vec)     for(auto &&it:vec) {cin >> it;}
+#define display_arr(vec)   for (auto &&it : vec){cout << it << " ";} cout << endl;
+#define files              freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define efficient          ios_base::sync_with_stdio(0); cin.tie(0);
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    ll n; cin >> n;
+    ll b = (n*(n-1)) / 2;
+    vll a(b);
+    input_arr(a);
+    sort(all(a));
+
+    vll ans;
+    ans.push_back(a[b-1]);
+    ll cnt = 1, pos = b-1;
+    for (ll i = b - 1; i >= 0; i--)
+    {
+        if (pos == i) {
+            ans.push_back(a[i]);
+            cnt++;
+            pos -= cnt;
+        }
+    }
+    
+    sort(all(ans));
+    display_arr(ans);
+}
+
+int main(void)
+{
+    //efficient;
+
+    int t = 1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
