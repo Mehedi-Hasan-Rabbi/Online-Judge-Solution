951cb63d7788876bf32d05a378613ee1af7b318c
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2023-08-29 16:34:37 +0600

    Solved 29/08/2023

---
+++ CodeForces/1761 B. Elimination of a Ring.cpp
@@ -0,0 +1,59 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+#define endl               '\n'
+#define yes                cout << "YES" << endl
+#define no                 cout << "NO" << endl
+#define show(x)            cout << #x << " : " << x << endl
+#define all(a)             a.begin(),a.end()
+#define pb                 push_back
+#define pf                 push_front
+#define mp                 make_pair
+#define ff                 first
+#define ss                 second
+#define input_arr(vec)     for(auto &&it:vec) {cin >> it;}
+#define display_arr(vec)   for (auto &&it : vec){cout << it << " ";} cout << endl;
+#define files              freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define efficient          ios_base::sync_with_stdio(0); cin.tie(0);
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    int n; cin >> n;
+    vi a(n);
+    input_arr(a);
+
+    set<int> s;
+    for (int i = 0; i < n; i++)
+        s.insert(a[i]);
+    
+    if (n == 1) cout << 1 << endl;
+    else if (s.size() == 2) cout << ((n - 2) / 2) + 2 << endl;
+    else cout << n << endl; 
+    
+}
+
+int main(void)
+{
+    efficient;
+
+    int t = 1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
