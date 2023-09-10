3ca2c4d2198fd6d7a8a545394e1028e9a8c424c6
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2023-09-11 01:04:21 +0600

    Solved 11/09/2023

---
+++ CodeForces/1869 A. Make It Zero.cpp
@@ -0,0 +1,76 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+#define endl               '\n'
+#define yes                cout << "YES" << endl
+#define no                 cout << "NO" << endl
+#define show(x)            cout << #x << " : " << x << endl
+#define showtwo(x, y)      cout << #x << " : " << x << " " << #y << " : " << y << endl
+#define all(a)             a.begin(),a.end()
+#define pb                 push_back
+#define pf                 push_front
+#define mp                 make_pair
+#define ff                 first
+#define ss                 second
+#define input_arr(vec)     for(auto &&it:vec) {cin >> it;}
+#define display_arr(vec)   for (auto &&it : vec){cout << it << " ";} cout << endl;
+#define files              freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define efficient          ios_base::sync_with_stdio(0); cin.tie(0);
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    int n; cin >> n;
+    vi arr(n);
+    input_arr(arr);
+
+    int tot_x_or = 0;
+    for (int i = 0; i < n; i++)
+        tot_x_or = tot_x_or ^ arr[i];
+    
+    if (tot_x_or == 0) {
+        cout << 2 << endl;
+        cout << 1 << " " << n << endl;
+        cout << 1 << " " << n << endl;
+        return;
+    }
+
+    else if (n % 2 == 0) {
+        cout << 2 << endl;
+        cout << 1 << " " << n << endl;
+        cout << 1 << " " << n << endl;
+    }
+    
+    else if (n % 2 == 1) {
+        cout << 4 << endl;
+        cout << 1 << " " << 2 << endl;
+        cout << 1 << " " << 2 << endl;
+        cout << 2 << " " << n << endl;
+        cout << 2 << " " << n << endl; 
+    }
+}
+
+int main(void)
+{
+    //efficient;
+
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++)
+    {
+        solve();
+    }
+    return 0;
+}
