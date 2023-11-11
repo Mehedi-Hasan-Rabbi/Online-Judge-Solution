b81051c7158f348b09bac04ea6a42f3b5ae9a2a0
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2023-11-11 10:21:43 +0600

    Solved 11/11/2023

---
+++ CodeForces/1858 A. Buttons.cpp
@@ -0,0 +1,59 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+#define endl               '\n'
+#define yes                cout << "YES" << endl
+#define no                 cout << "NO" << endl
+#define show(x)            cout << #x << " : " << x << endl
+#define showtwo(x, y)      cout << #x << " : " << x << " " << #y << " : " << y << endl
+#define all(a)             a.begin(),a.end()
+#define pb                 push_back
+#define pf                 push_front
+#define mp                 make_pair
+#define ff                 first
+#define ss                 second
+#define input_arr(vec)     for(auto &&it:vec) {cin >> it;}
+#define display_arr(vec)   for (auto &&it : vec){cout << it << " ";} cout << endl;
+#define files              freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    int a, k, both; cin >> a >> k >> both;
+
+    if (both % 2 == 1) k--;
+
+    if (a <= k) cout << "Second" << endl;
+    else if (a > k) cout << "First" << endl;
+}
+
+int main(void)
+{
+    //efficient();
+
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++)
+    {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+    return 0;
+}
