8af0fad83768c202a1177d8ed11cc3e327b54346
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2023-12-11 11:36:05 +0600

    Solved 10/12/2023

---
+++ CodeForces/1904 B. Collecting_Game.cpp
@@ -0,0 +1,116 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+#define endl               '\n'
+#define yes                cout << "YES" << endl
+#define no                 cout << "NO" << endl
+#define show(x)            cout << #x << " : " << x << endl
+#define showtwo(x, y)      cout << #x << " : " << x << " " << #y << " : " << y << endl
+#define all(a)             (a).begin(),(a).end()
+#define pb                 push_back
+#define pf                 push_front
+#define mp                 make_pair
+#define ff                 first
+#define ss                 second
+#define mem(a, b)          memset(a, b, sizeof(a) )
+#define input_arr(vec)     for(auto &&it:vec) {cin >> it;}
+#define display_arr(vec)   for (auto &&it : vec){cout << it << " ";} cout << endl;
+#define files()            freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+
+template < typename F, typename S >ostream& operator << ( ostream& os, const pair< F, S > & p ) {return os << "(" << p.first << ", " << p.second << ")";}
+template < typename T >ostream &operator << ( ostream & os, const vector< T > &v ) {os << "{";for(auto it = v.begin(); it != v.end(); ++it) {if( it != v.begin() ) os << ", ";os << *it;}return os << "}";}
+template < typename T >ostream &operator << ( ostream & os, const set< T > &v ) {os << "[";for(auto it = v.begin(); it != v.end(); ++it) {if( it != v.begin() ) os << ", ";os << *it;}return os << "]";}
+template < typename T >ostream &operator << ( ostream & os, const multiset< T > &v ) {os << "[";for(auto it = v.begin(); it != v.end(); ++it) {if( it != v.begin() ) os << ", ";os << *it;}return os << "]";}
+template < typename F, typename S >ostream &operator << ( ostream & os, const map< F, S > &v ) {os << "[";for(auto it = v.begin(); it != v.end(); ++it) {if( it != v.begin() ) os << ", ";os << it -> first << " = " << it -> second ;}return os << "]";}
+ 
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+ 
+void faltu () {cerr << endl;}
+template <typename T>void faltu( T a[], int n ) {for(int i = 0; i < n; ++i) cerr << a[i] << ' ';cerr << endl;}
+template <typename T, typename ... hello>void faltu( T arg, const hello &... rest) {cerr << arg << ' ';faltu(rest...);}
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    int n; cin >> n;
+    vector<pll> a;
+    vll pref;
+    vll arr;
+    vll ans(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        ll x; cin >> x;
+        a.push_back( {x, i} );
+        arr.push_back(x);
+        pref.push_back(x);
+    }
+    sort(all(a)); sort(all(pref)); sort(all(arr));
+
+    for (int i = 1; i < n; i++) pref[i] = pref[i] + pref[i - 1];
+
+    for (int i = 0; i < n; i++)
+    {
+        ll cnt = i;
+        ll curr = pref[i];
+        ll id = i;
+
+        // dbg(id, curr, cnt);
+        // cout << "start:" << endl;
+
+        set<ll> idcnt;
+        idcnt.insert(id);
+        ll sz = idcnt.size();
+
+        while (true)
+        {
+            id = ( upper_bound(arr.begin() + id, arr.end(), curr) ) - arr.begin();
+        
+            if (id <= n) {
+                idcnt.insert(id);
+                if (sz < idcnt.size()) {
+                    curr = pref[id - 1];
+                    cnt = id - 1;
+                    sz = idcnt.size();
+                }
+                else break;
+            }
+        }
+        ans[ a[i].second ] = cnt;
+    }
+    
+    display_arr(ans);
+    
+}
+
+int main(void)
+{
+    efficient();
+
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++)
+    {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+    return 0;
+}
