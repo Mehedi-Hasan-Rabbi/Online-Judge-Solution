90f66aaab8b06d998041761e670ae70d30b229b0
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2024-03-12 23:49:07 +0600

    Solved 12/03/2024

---
+++ CodeForces/1935 A. Entertainment in MAC.cpp
@@ -0,0 +1,90 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+#define endl               '\n'
+#define yes                cout << "YES" << endl
+#define no                 cout << "NO" << endl
+#define show(x)            cout << #x << " : " << x << endl
+#define showtwo(x, y)      cout << #x << " : " << x << " " << #y << " : " << y << endl
+#define all(a)             (a).begin(),(a).end()
+#define rall(a)            (a).rbegin(),(a).rend()
+#define toLowerCase(s)     transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define toUpperCase(s)     transform(s.begin(), s.end(), s.begin(), ::toupper);
+#define pb                 push_back
+#define pf                 push_front
+#define mp                 make_pair
+#define ff                 first
+#define ss                 second
+#define mem(a, b)          memset(a, b, sizeof(a) )
+#define input_arr(vec)     for(auto &&it:vec) {cin >> it;}
+#define display_arr(vec)   for (auto &&it : vec){cout << it << " ";} cout << endl;
+#define files()            freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+
+template < typename F, typename S >ostream& operator << ( ostream& os, const pair< F, S > & p ) {return os << "(" << p.first << ", " << p.second << ")";}
+template < typename T >ostream &operator << ( ostream & os, const vector< T > &v ) {os << "{";for(auto it = v.begin(); it != v.end(); ++it) {if( it != v.begin() ) os << ", ";os << *it;}return os << "}";}
+template < typename T >ostream &operator << ( ostream & os, const set< T > &v ) {os << "[";for(auto it = v.begin(); it != v.end(); ++it) {if( it != v.begin() ) os << ", ";os << *it;}return os << "]";}
+template < typename T >ostream &operator << ( ostream & os, const multiset< T > &v ) {os << "[";for(auto it = v.begin(); it != v.end(); ++it) {if( it != v.begin() ) os << ", ";os << *it;}return os << "]";}
+template < typename F, typename S >ostream &operator << ( ostream & os, const map< F, S > &v ) {os << "[";for(auto it = v.begin(); it != v.end(); ++it) {if( it != v.begin() ) os << ", ";os << it -> first << " = " << it -> second ;}return os << "]";}
+ 
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+ 
+void faltu () {cerr << endl;}
+template <typename T>void faltu( T a[], int n ) {for(int i = 0; i < n; ++i) cerr << a[i] << ' ';cerr << endl;}
+template <typename T, typename ... hello>void faltu( T arg, const hello &... rest) {cerr << arg << ' ';faltu(rest...);}
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    int n; cin >> n;
+    string s; cin >> s;
+
+    int l = 0, r = s.size() - 1, common = 0;
+    while (l < r)
+    {
+        if (s[l] == s[r]) common++;
+        if (s[r] < s[l] and common == l) {
+            string temp = s;
+            reverse(s.begin(), s.end());
+            s += temp;
+            cout << s << endl;
+            return;
+        }
+        l++, r--;
+    }
+    cout << s << endl;
+}
+
+int main(void)
+{
+    // efficient();
+    // clock_t z = clock();
+
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++)
+    {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+
+    // cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC); // the run time printed here is in seconds
+    return 0;
+}
