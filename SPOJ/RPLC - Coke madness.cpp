db23a9a82548369acc3c4f93d0efae0dda4dacf3
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2024-04-20 19:27:53 +0600

    Solved 20/04/2024

---
+++ SPOJ/RPLC - Coke madness.cpp
@@ -0,0 +1,70 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+
+/*---------------------------------------------------------------------------------------------------------------*/
+const int mx = 1e6+123;
+int a[mx], n;
+
+bool check (ll val) {
+    ll score = val;
+    for (int i = 1; i <= n; i++) {
+        score += a[i];
+        if (score <= 0) return false;
+    }
+    return true;
+}
+
+void solve()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+
+    ll l = 1, r = 1e14+123;
+    ll ans;
+
+    while (l <= r) {
+        ll mid = l + (r - l) / 2;
+        if (check(mid)) {
+            ans = mid;
+            r = mid - 1;
+        }
+        else l = mid + 1;
+    }
+    cout << ans << endl;
+}
+
+int main(void) {
+    efficient();
+
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        cout << "Scenario #" << tc << ": ";
+        solve();
+    }
+
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
