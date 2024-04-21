389f91f5af7eb1e8355cbf56c0473faedcb69d26
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2024-04-22 05:56:26 +0600

    Solved 22/04/2024

---
+++ CodeForces/1957 B. A BIT of a Construction.cpp
@@ -0,0 +1,66 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    int n, k; cin >> n >> k;
+    int bit = log2(k) + 1;
+    int sum  = 0, curr = 0;
+
+    vector<int> ans;
+    if (n > 1) {
+        n--;
+        for (int i = 0; i < bit; i++) {
+            int val = (1 << i);
+            if (val <= k) {
+                sum += val;
+                k -= val;
+            }
+        }
+        ans.push_back(sum);
+    }
+    ans.push_back(k); n--;
+
+    while (n--) ans.push_back(0);
+    
+    for (auto &&u: ans) cout << u << " ";
+    cout << endl;
+}
+
+int main(void) {
+    // efficient();
+
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
