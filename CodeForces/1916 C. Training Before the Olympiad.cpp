67cafa6cea8df4dc5734127e80f168549e958e8a
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2024-05-01 14:56:21 +0600

    Solved 01/05/2024

---
+++ CodeForces/1916 C. Training Before the Olympiad.cpp
@@ -0,0 +1,57 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    int n; cin >> n;
+    vector<int> a(n, 0);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    ll sum = 0; int odd = 0;
+    for (int i = 0; i < n; i++) {
+        sum += a[i];
+        odd += (a[i] % 2 == 1);
+
+        if (i == 0) cout << sum << " ";
+        else cout << sum - (odd / 3) - (odd % 3 == 1) << " ";
+    }
+    cout << endl;
+}
+
+int main(void) {
+    efficient();
+
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
