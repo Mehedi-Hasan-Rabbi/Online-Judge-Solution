b8e26329d8fcfe960668b1555b5e7ecdea873311
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2024-05-01 18:40:12 +0600

    Solved 01/05/2024

---
+++ CodeForces/1914 C. Quests.cpp
@@ -0,0 +1,62 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    ll n, k; cin >> n >> k;
+    vector<ll> a(n, 0), b(n, 0);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> b[i];
+
+    ll experience = -1, max_b = -1;
+    for (int i = 1; i < n; i++) a[i] = a[i] + a[i - 1];
+    
+    for (int i = 0; i < min(n, k); i++) {
+        ll sum = a[i];
+        ll remain_k = k - i - 1;
+        max_b = max(b[i], max_b);
+        
+        sum = sum + (remain_k * max_b);
+        experience = max(experience, sum);
+    }
+
+    cout << experience << endl;
+}
+
+int main(void) {
+    efficient();
+
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
