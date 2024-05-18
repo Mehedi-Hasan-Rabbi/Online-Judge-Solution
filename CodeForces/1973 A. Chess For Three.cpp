e145a681cba0ac2c3aa6c65fba40ad0f607d8a2f
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2024-05-19 00:34:53 +0600

    Solved 18/05/2024

---
+++ CodeForces/1973 A. Chess For Three.cpp
@@ -0,0 +1,58 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    int a[3]; cin >> a[0] >> a[1] >> a[2];
+
+    if ((a[0]+a[1]+a[2]) % 2 != 0) cout << -1 << endl; // Coz, every time 2 or (1+1 = 2) is incremented
+    else {
+        int ans = 0;
+        while (!(a[0] == 0 and a[1] == 0)) {
+            a[1]--;
+            a[2]--;
+            sort(a, a+3);
+            ans++;
+        }
+        cout << ans << endl;
+    }
+        
+}
+
+int main(void) {
+    efficient();
+
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
