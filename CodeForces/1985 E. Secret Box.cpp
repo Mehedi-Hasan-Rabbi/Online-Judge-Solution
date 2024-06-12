8f63bf57ec02b2f62acd6076906b1ab6032485d9
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2024-06-12 10:22:38 +0600

    Solved 12/05/2024

---
+++ CodeForces/1985 E. Secret Box.cpp
@@ -0,0 +1,62 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    ll x, y, z, k; cin >> x >> y >> z >> k;
+    ll ans = 0, curr = 0;
+
+    for (ll sx = 1; sx <= x; sx++)
+    {
+        for (ll sy = 1; sy <= y; sy++)
+        {
+            if (k % (sx * sy) != 0) continue;
+
+            ll sz = k / (sx * sy);
+            if (sz > z) continue;
+
+            curr = (x - sx + 1) * (y - sy + 1) * (z - sz + 1);
+            ans = max(ans, curr);
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main(void) {
+    efficient();
+
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
