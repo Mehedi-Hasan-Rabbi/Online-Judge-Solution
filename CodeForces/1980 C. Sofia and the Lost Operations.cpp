dcc418de977c30ec28f0a959c4e9b2261091395e
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2024-06-15 23:03:49 +0600

    Solved 15/05/2024

---
+++ CodeForces/1980 C. Sofia and the Lost Operations.cpp
@@ -0,0 +1,72 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    int n; cin >> n;
+    vector<int> a(n + 1, 0), b(n + 1, 0);
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    for (int i = 1; i <= n; i++) cin >> b[i];
+
+    int m; cin >> m;
+    vector<int> d(m + 1, 0);
+    for (int i = 1; i <= m; i++) cin >> d[i];
+
+    map<int, int> dif, freq;
+    for (int i = 1; i <= n; i++) {
+        if (a[i] != b[i])
+            dif[b[i]]++;
+    }
+    for (int i = 1; i <= m; i++) {
+        freq[d[i]]++;
+    }
+
+    bool ok = true;
+    for (auto [key, val]: dif) {
+        if (freq[key] < val)
+            ok = false;
+    }
+
+    if (count(b.begin(), b.end(), d[m]) == 0) ok = false;
+
+    if (ok) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
+int main(void) {
+    efficient();
+
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
