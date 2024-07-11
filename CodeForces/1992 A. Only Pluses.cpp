158eb3b6f96c1c42f0de2393f633f0a69b0bd06e
Mehedi-Hasan-Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2024-07-11 22:59:47 +0600

    Solved 11/07/2024

---
+++ CodeForces/1992 A. Only Pluses.cpp
@@ -0,0 +1,65 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+
+int dx[] = {+1, -1, 0, 0, +1, +1, -1, -1};
+int dy[] = {0, 0, -1, +1, +1, -1, +1, -1};
+
+const ll MOD = 1e9 + 7; // 1000000007;
+const double PI = acos(-1);
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    int a, b, c; cin >> a >> b >> c;
+    
+    int ans = 0, cnt = 0, val = 0;
+    for (int i = 0; i <= 5; i++) {
+        int x = a, y = b, z = c;
+        x = a + i;
+
+        for (int j = 0; i + j <= 5; j++) {
+            y = b + j;
+            for (int k = 0; i + j + k <= 5; k++) {
+                z = c + k;
+                val = x * y * z;
+                ans = max(ans, val);
+            }
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main(void) {
+    // efficient();
+
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
