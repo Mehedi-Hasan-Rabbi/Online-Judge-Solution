cedd0d6f26c08fe4d28dbd0f1d5adbe799e7ca0e
Mehedi Hasan Rabbi <mehedi.saiyan@gmail.com>
2025-01-19 23:11:15 +0600

    Solved 19/01/2025

---
+++ CodeForces/2060 D. Subtract Min Sort.cpp
@@ -0,0 +1,68 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+ 
+int dx[] = {+1, -1, 0, 0, +1, +1, -1, -1};
+int dy[] = {0, 0, -1, +1, +1, -1, +1, -1};
+ 
+const ll mod = 1e9 + 7; // 1000000007;
+const double PI = 3.14159265358979323846;
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+ 
+/*---------------------------------------------------------------------------------------------------------------*/
+ 
+void solve()
+{
+    int n; cin >> n;
+    vector<int> a(n, 0);
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    for (int i = 0; i < n - 1; i++) {
+        int val = min(a[i], a[i + 1]);
+
+        a[i] -= val; a[i + 1] -=  val;
+
+        if (a[i] < 0 or a[i + 1] < 0) {
+            cout << "NO" << endl;
+            return;
+        }
+    }
+
+    if (is_sorted(a.begin(), a.end()))
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+ 
+int main(void) {
+    efficient();
+ 
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+ 
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
