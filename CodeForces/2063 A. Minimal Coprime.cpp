1620902598fa95807e9ad89b652e43d7442af956
Mehedi Hasan Rabbi <mehedi.saiyan@gmail.com>
2025-01-23 23:33:36 +0600

    Solved 22/01/2025

---
+++ CodeForces/2063 A. Minimal Coprime.cpp
@@ -0,0 +1,54 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+ 
+int dx[] = {+1, -1, 0, 0, +1, +1, -1, -1};
+int dy[] = {0, 0, -1, +1, +1, -1, +1, -1};
+ 
+const ll mod = 1e9 + 7; // 1000000007;
+const double PI = 3.14159265358979323846;
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+ 
+/*---------------------------------------------------------------------------------------------------------------*/
+ 
+void solve()
+{
+    ll l, r; cin >> l >> r;
+
+    if (l == r) {
+        if (gcd(l, r) == 1) cout << 1 << endl;
+        else cout << 0 << endl;
+    }
+    else cout << r - l << endl;
+}
+ 
+int main(void) {
+    efficient();
+ 
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+ 
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
