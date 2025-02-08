0e1b6e1fbd264613775cfb07e1f1a41a8e529460
Mehedi Hasan Rabbi <mehedi.saiyan@gmail.com>
2025-02-08 11:33:41 +0600

    Solved 08/02/2025

---
+++ CodeForces/2040 B. Paint a Strip.cpp
@@ -0,0 +1,73 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+ 
+int dx[] = {+1, -1, 0, 0, +1, +1, -1, -1};
+int dy[] = {0, 0, -1, +1, +1, -1, +1, -1};
+ 
+const ll mod = 1e9 + 7; // 1000000007;
+const double PI = 3.14159265358979323846;
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+ 
+/*---------------------------------------------------------------------------------------------------------------*/
+ 
+void solve()
+{
+    int n; cin >> n;
+
+    if (n == 1) {cout << 1 << endl; return;}
+    else if (n <= 4) {cout << 2 << endl; return;}
+    
+    int sum = 1, ans = 1;
+    int l = 1, r = n;
+    while (sum < n) {
+        int idx = -1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            int val = (mid / 2) + (mid % 2 != 0);
+            if (sum + 1 >=  val) {
+                l = mid + 1;
+                idx = mid;
+            }
+            else r = mid - 1;
+        }
+        l = 1, r = n;
+        sum = idx;
+        ans++;
+    }
+
+    cout << ans << endl;
+}
+ 
+int main(void) {
+    efficient();
+ 
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+ 
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
