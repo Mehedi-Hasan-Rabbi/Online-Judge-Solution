33984e67268043eb040eea396aa6371c85d5ab7e
Mehedi Hasan Rabbi <mehedi.saiyan@gmail.com>
2025-02-22 22:15:21 +0600

    Solved 21/02/2025

---
+++ CodeForces/2065 C1. Skibidus and Fanum Tax (easy version).cpp
@@ -0,0 +1,68 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+ 
+int dx[] = {+1, -1, 0, 0, +1, +1, -1, -1};
+int dy[] = {0, 0, -1, +1, +1, -1, +1, -1};
+ 
+const ll mod = 1e9 + 7; // 1000000007;
+const double PI = 3.14159265358979323846;
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+ 
+/*---------------------------------------------------------------------------------------------------------------*/
+ 
+void solve()
+{
+    int n, m; cin >> n >> m;
+    vector<ll> a(n, 0), b(m, 0);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < m; i++) cin >> b[i];
+
+    a[0] = min(a[0], (b[0] - a[0]));
+
+    for (int i = 1; i < n; i++) {
+        int mn = min(a[i], (b[0] - a[i]));
+        int mx = max(a[i], (b[0] - a[i]));
+        
+        if (a[i - 1] <= mn)
+            a[i] = mn;
+        else if (a[i - 1] <= mx)
+            a[i] = mx;
+        else {
+            cout << "NO" << endl;
+            return;
+        }
+    }
+    cout << "YES" << endl;
+}
+ 
+int main(void) {
+    efficient();
+ 
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+ 
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
