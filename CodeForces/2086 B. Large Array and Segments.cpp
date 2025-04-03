c4e15e1fb1290d7d609d207c39cfbeb475c1ae17
Mehedi Hasan Rabbi <mehedi.saiyan@gmail.com>
2025-04-03 22:38:39 +0600

    Solved 03/04/2025

---
+++ CodeForces/2086 B. Large Array and Segments.cpp
@@ -0,0 +1,86 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+ 
+int dx[] = {+1, -1, 0, 0, +1, +1, -1, -1};
+int dy[] = {0, 0, -1, +1, +1, -1, +1, -1};
+ 
+const ll mod = 1e9 + 7; // 1000000007;
+const double PI = 3.14159265358979323846;
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+ 
+/*---------------------------------------------------------------------------------------------------------------*/
+ 
+void solve()
+{
+    ll n, k, x; cin >> n >> k >> x;
+    vector<ll> a(n + 1, 0);
+    ll sum = 0LL;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        sum += a[i];
+    }
+
+    ll l = 1, r = k, seg = -1;
+    while (l <= r) {
+        ll mid = l + (r - l) / 2;
+
+        if ( (mid * sum) >= x ) {
+            seg = mid;
+            r = mid - 1;
+        }
+        else l = mid + 1;
+    }
+
+    if (seg == -1) {
+        cout << 0 << endl;
+        return;
+    }
+
+    ll tot_idx = n * k;
+    ll left_idx = (n * (seg - 1));
+    sum = sum * (seg - 1);
+
+
+    for (int i = n; i >= 1; i--) {
+        sum += a[i];
+
+        if (sum >= x) {
+            break;
+        }
+        left_idx++;
+    }
+
+    cout << tot_idx - left_idx << endl;
+}
+ 
+int main(void) {
+    // efficient();
+ 
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+ 
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
