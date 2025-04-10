c9331f5d8b58bd7ea4f57b8175f5ad491516dc77
Mehedi Hasan Rabbi <mehedi.saiyan@gmail.com>
2025-04-10 11:26:44 +0600

    Solved 08/04/2025

---
+++ CodeForces/2093 B. Expensive Number.cpp
@@ -0,0 +1,74 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+ 
+int dx[] = {+1, -1, 0, 0, +1, +1, -1, -1};
+int dy[] = {0, 0, -1, +1, +1, -1, +1, -1};
+ 
+const ll mod = 1e9 + 7; // 1000000007;
+const double PI = 3.14159265358979323846;
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+ 
+/*---------------------------------------------------------------------------------------------------------------*/
+ 
+void solve()
+{
+    string s; cin >> s;
+    int n = (int) s.size();
+
+    int zero = 0, non_zero = 0;
+    for (auto u: s) {
+        if (u == '0') zero++;
+        else non_zero++;
+    }
+
+    if (zero) {
+        int idx = -1;
+        for (int i = 0; i < n; i++) {
+            if (s[i] != '0') {
+                idx = i;
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < idx; i++) {
+            if (s[i] != '0')
+                ans++;
+        }
+        
+        ans += (n - idx - 1);
+        cout << ans << endl;
+    }
+    else cout << non_zero - 1 << endl;
+}
+ 
+int main(void) {
+    efficient();
+ 
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+ 
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
