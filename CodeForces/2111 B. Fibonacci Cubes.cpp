baed748fc2a13905c6c93234c4c073807fd04de7
Mehedi Hasan Rabbi <mehedi.saiyan@gmail.com>
2025-06-11 23:16:06 +0600

    Solved 11/06/2025

---
+++ CodeForces/2111 B. Fibonacci Cubes.cpp
@@ -0,0 +1,64 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+ 
+int dx[] = {+1, -1, 0, 0, +1, +1, -1, -1};
+int dy[] = {0, 0, -1, +1, +1, -1, +1, -1};
+ 
+const ll mod = 1e9 + 7; // 1000000007;
+const double PI = 3.14159265358979323846;
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+ 
+/*---------------------------------------------------------------------------------------------------------------*/
+ 
+void solve()
+{
+    int n, m; cin >> n >> m;
+    vector<int> a(n, 0);
+    a[0] = 1, a[1] = 2;
+    for (int i = 2; i < n; i++)
+        a[i] = a[i - 1] + a[i - 2];
+
+    while (m--) {
+        int w, l, h; cin >> w >> l >> h;
+
+        int mn = min({w, l, h}), mx = max({w, l, h});
+
+        if ( (mn >= a[n - 1]) and (mx - a[n - 1]) >= a[n - 2] )
+            cout << 1;
+        else 
+            cout << 0;
+    }
+    cout << endl;
+}
+ 
+int main(void) {
+    efficient();
+ 
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+ 
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
