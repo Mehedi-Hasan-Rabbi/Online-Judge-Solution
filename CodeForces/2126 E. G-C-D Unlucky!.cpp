3ad8d2567b19c3a9bfb0517f5861458c6be0fba8
Mehedi Hasan Rabbi <mehedi.saiyan@gmail.com>
2025-07-21 20:36:45 +0600

    Solved 21/07/2025

---
+++ CodeForces/2126 E. G-C-D Unlucky!.cpp
@@ -0,0 +1,78 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+ 
+int dx[] = {+1, -1, 0, 0, +1, +1, -1, -1};
+int dy[] = {0, 0, -1, +1, +1, -1, +1, -1};
+ 
+const ll mod = 1e9 + 7; // 1000000007;
+const double PI = 3.14159265358979323846;
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+ 
+/*---------------------------------------------------------------------------------------------------------------*/
+ 
+void solve()
+{
+    int n; cin >> n;
+    vector<ll> pref (n + 1, 0), suff (n + 1, 0);
+    for (int i = 1; i <= n; i++) cin >> pref[i];
+    for (int i = 1; i <= n; i++) cin >> suff[i];
+
+    vector<ll> a(n + 1, 0);
+    for (int i = 1; i <= n; i++) {
+        a[i] = lcm(pref[i], suff[i]);
+    }
+
+    ll GCD = a[1];
+    for (int i = 1; i <= n; i++) {
+        GCD = gcd(GCD, a[i]);
+ 
+        if (GCD != pref[i]) {
+                cout << "NO" << endl;
+                return;
+        }
+    }
+
+    GCD = a[n];
+    for (int i = n; i >= 1; i--) {
+        GCD = gcd(GCD, a[i]);
+    
+        if (GCD != suff[i]) {
+            cout << "NO" << endl;
+            return;
+        }
+    }
+ 
+    cout << "YES" << endl;
+}
+ 
+int main(void) {
+    efficient();
+ 
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+ 
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
