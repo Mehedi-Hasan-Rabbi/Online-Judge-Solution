009a2fe78169cf85c478054bf52a7183408dce31
Mehedi Hasan Rabbi <mehedi.saiyan@gmail.com>
2025-07-24 09:10:37 +0600

    Solved 23/07/2025

---
+++ CodeForces/2125 A. Difficult Contest.cpp
@@ -0,0 +1,60 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+ 
+int dx[] = {+1, -1, 0, 0, +1, +1, -1, -1};
+int dy[] = {0, 0, -1, +1, +1, -1, +1, -1};
+ 
+const ll mod = 1e9 + 7; // 1000000007;
+const double PI = 3.14159265358979323846;
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+ 
+/*---------------------------------------------------------------------------------------------------------------*/
+ 
+void solve()
+{
+    string s; cin >> s;
+    int n = (int) s.size();
+    sort(s.begin(), s.end(), greater<char>());
+    // for (int i = 0; i + 2 < n; i++) {
+    //     string sub = s.substr(i, 3);
+
+    //     if (sub == "FFT")
+    //         swap(s[i+1], s[i+2]);
+    //     if (sub == "NTT")
+    //         swap(s[i], s[i+1]);
+    // }
+
+    cout << s << endl;
+}
+ 
+int main(void) {
+    // efficient();
+ 
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+ 
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
