175594c719105b4ced0c679df1a883af7a18bd6f
Mehedi Hasan Rabbi <mehedi.saiyan@gmail.com>
2025-07-31 16:01:34 +0600

    Solved 31/07/2025

---
+++ CodeForces/2118 A. Equal Subsequences.cpp
@@ -0,0 +1,61 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+ 
+int dx[] = {+1, -1, 0, 0, +1, +1, -1, -1};
+int dy[] = {0, 0, -1, +1, +1, -1, +1, -1};
+ 
+const ll mod = 1e9 + 7; // 1000000007;
+const double PI = 3.14159265358979323846;
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+ 
+/*---------------------------------------------------------------------------------------------------------------*/
+ 
+void solve()
+{
+    int n, k; cin >> n >> k;
+    string s(n, '0');
+
+    if (k) s[0] = '1', k--;
+    int i = n - 2;
+
+    while (s[i] == '0' and k > 0 and i >= 0) {
+        s[i] = '1';
+        k--, i--;
+    }
+
+    if (k) s[n - 1] = '1', k--;
+
+    cout << s << endl;
+}
+ 
+int main(void) {
+    efficient();
+ 
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+ 
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
