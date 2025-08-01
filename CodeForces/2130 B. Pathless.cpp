ab7ca5b2d5b3bd02431d460de415091050aac88f
Mehedi Hasan Rabbi <mehedi.saiyan@gmail.com>
2025-08-01 23:08:23 +0600

    Solved 01/08/2025

---
+++ CodeForces/2130 B. Pathless.cpp
@@ -0,0 +1,68 @@
+// In the name of Allah the merciful.
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+ 
+int dx[] = {+1, -1, 0, 0, +1, +1, -1, -1};
+int dy[] = {0, 0, -1, +1, +1, -1, +1, -1};
+ 
+const ll mod = 1e9 + 7; // 1000000007;
+const double PI = 3.14159265358979323846;
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+ 
+/*---------------------------------------------------------------------------------------------------------------*/
+ 
+void solve()
+{
+    int n, s; cin >> n >> s;
+    int count0 = 0, count1 = 0, count2 = 0;
+
+    for (int i = 1; i <= n; i++) {
+        int x; cin >> x;
+
+        if (x == 0) count0++;
+        if (x == 1) count1++;
+        if (x == 2) count2++;
+    }
+
+    int sum = (count0 * 0) + (count1 * 1) + (count2 * 2);
+
+    if (s - sum == 1 or s < sum) {
+        for (int i = 1; i <= count0; i++) cout << 0 << " ";
+        for (int i = 1; i <= count2; i++) cout << 2 << " ";
+        for (int i = 1; i <= count1; i++) cout << 1 << " ";
+        cout << endl;
+    }
+    else
+        cout << -1 << endl;
+}
+ 
+int main(void) {
+    efficient();
+ 
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+ 
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
