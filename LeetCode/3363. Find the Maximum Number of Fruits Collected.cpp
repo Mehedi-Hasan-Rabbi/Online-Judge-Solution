93c54edad36cd59790a56068a968b485de11c162
Mehedi Hasan Rabbi <mehedi.saiyan@gmail.com>
2025-08-08 14:56:43 +0600

    Add alternative solution for max fruits collection problem
    Introduced a second solution class for the 'Find the Maximum Number of Fruits Collected' problem, implementing a different dynamic programming approach. This provides an alternative method for solving the problem and includes additional comments and references for clarity.

--- LeetCode/3363. Find the Maximum Number of Fruits Collected.cpp
+++ LeetCode/3363. Find the Maximum Number of Fruits Collected.cpp
@@ -1,3 +1,4 @@
+// Solution - 1
 class Solution {
 public:
     int n;
@@ -70,4 +71,67 @@ public:
     }
 
     // Editorial (Just take the concept): https://www.youtube.com/watch?v=HkjfJKlxS9U
+};
+
+// Solution - 2
+class Solution {
+public:
+    int n;
+    int dx[3], dy[3];
+    vector<vector<int>> dp;
+
+    int find_max (int row, int col, int move_left, vector<vector<int>>& fruits) {
+        if (row == n - 1 and col == n - 1) {
+            if (move_left == 0) return 0; // Found a path with valid move
+            else return INT_MIN; // Did not found a path with valid move
+        }
+        if (move_left == 0) return INT_MIN; // Used all move but could not found path
+        if (dp[row][col] != -1) return dp[row][col];
+
+        // maxFruits = INT_MIN, Because If we can't find and valid path then maxFruits value will not change and that means no path found.
+
+        int maxFruits = INT_MIN, collectedFruits = fruits[row][col];
+        for (int i = 0; i < 3; i++) {
+            int newRow = row + dx[i];
+            int newCol = col + dy[i];
+
+            if ( (0 <= newRow and newRow < n) and (0 <= newCol and newCol < n) ) {
+                int val = find_max(newRow, newCol, move_left - 1, fruits);
+
+                if (val != INT_MIN)
+                    maxFruits = max(maxFruits, collectedFruits + val);
+            }
+        }
+
+        return dp[row][col] = maxFruits;
+    }
+
+    int maxCollectedFruits(vector<vector<int>>& fruits) {
+        n = (int) fruits.size();
+        int ans = 0;
+
+        // Child (0, 0) --> TopLeft
+        for (int i = 0; i < n; i++) {
+            ans += fruits[i][i];
+            fruits[i][i] = 0;
+        }
+
+        // Child (0, n - 1) --> TopRight
+        dp.assign(n, vector<int>(n, -1));
+        dx[0] = +1; dx[1] = +1; dx[2] = +1;
+        dy[0] = -1; dy[1] = 0; dy[2] = +1;
+
+        ans += find_max(0, n - 1, n - 1, fruits);
+
+        // Child (n - 1, 0) --> BottomLeft
+        dp.assign(n, vector<int>(n, -1));
+        dx[0] = -1; dx[1] = 0; dx[2] = +1;
+        dy[0] = +1; dy[1] = +1; dy[2] = +1;
+
+        ans += find_max(n - 1, 0, n - 1, fruits);
+
+        return ans;
+    }
+
+    // Editorial (Concept): https://www.youtube.com/watch?v=wTBrGvEU8mk
 };
