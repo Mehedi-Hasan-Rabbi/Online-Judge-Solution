9e9ee06325672900bc48c37f67d8b0f31fd3e436
Mehedi Hasan Rabbi <mehedi.saiyan@gmail.com>
2026-01-23 22:12:21 +0600

    Solved 23/01/2026

---
+++ CodeForces/2185 E. The Robotic Rush.cpp
@@ -0,0 +1,109 @@
+// In the name of Allah, the merciful.
+#include<bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#ifndef ONLINE_JUDGE
+#include "algo/Debug.h"
+#else
+#define dbg(...)
+#endif
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set; // *X.find_by_order(n), X.order_of_key(n), *X.lower_bound(n), *X.upper_bound(n), X.erase(n)
+// less<int> -> Ascending || greater<int> -> Descending || less_equal<int> -> Ascending Multiset || greater_equal<int> -> Descending Multiset
+ 
+#define endl               '\n'
+#define efficient()        ios_base::sync_with_stdio(0); cin.tie(0);
+#define fraction()         cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return abs(a) * ( abs(b) / gcd ( a, b ) ); }
+ 
+int dx[] = {+1, -1, 0, 0, +1, +1, -1, -1};
+int dy[] = {0, 0, -1, +1, +1, -1, +1, -1};
+ 
+const ll mod = 1e9 + 7; // 1000000007;
+const double PI = 3.14159265358979323846;
+const double eps = 1e-9; // if (abs(a-b) < 1e-9) {a and b are equal}
+ 
+/*---------------------------------------------------------------------------------------------------------------*/
+ 
+void solve()
+{
+    int n, m, k; cin >> n >> m >> k;
+    vector<int> robot(n + 1, 0), spike(m + 1, 0);
+    for (int i = 1; i <= n; i++) cin >> robot[i];
+    for (int i = 1; i <= m; i++) cin >> spike[i];
+    sort(spike.begin(), spike.end());
+
+    string op; cin >> op; op = "#" + op;
+    vector<int> left_pref(k + 1, 0), right_pref(k + 1, 0);
+
+    int initial = 0;
+    for (int i = 1; i <= k; i++) {
+        if (op[i] == 'L') initial--;
+        if (op[i] == 'R') initial++;
+
+        if (initial < 0) {
+            left_pref[i] = max(left_pref[i - 1], abs(initial));
+            right_pref[i] = right_pref[i - 1];
+        }
+        if (initial >= 0) {
+            right_pref[i] = max(right_pref[i - 1], initial);
+            left_pref[i] = left_pref[i - 1];
+        }
+    }
+
+    vector<int> diedAtOp(k + 2, 0);
+    
+    for (int i = 1; i <= n; i++) {
+        int closeSpikeLeft = -1, closeSpikeRight = -1;
+
+        // Find the left and right closest spike of a robot.
+        int idx = lower_bound(spike.begin(), spike.end(), robot[i]) - spike.begin();
+        if (idx - 1 >= 1) closeSpikeLeft = robot[i] - spike[idx - 1];
+
+        idx = upper_bound(spike.begin(), spike.end(), robot[i]) - spike.begin();
+        if (idx <= m) closeSpikeRight = spike[idx] - robot[i];
+
+        // At which operation this robot will die?
+        int op_num = INT32_MAX;
+        if (closeSpikeLeft != -1) {
+            int val = lower_bound(left_pref.begin(), left_pref.end(), closeSpikeLeft) - left_pref.begin();
+            if (1 <= val and val <= k) op_num = min(op_num, val);
+        }
+        if (closeSpikeRight != -1) {
+            int val = lower_bound(right_pref.begin(), right_pref.end(), closeSpikeRight) - right_pref.begin();
+            if (1 <= val and val <= k) op_num = min(op_num, val);
+        }
+
+        if (op_num != INT32_MAX) diedAtOp[op_num]++;
+
+    }
+    
+    for (int i = 1; i <= k; i++) {
+        n = n - diedAtOp[i];
+        cout << n << " ";
+    } cout << endl;
+}
+ 
+int main(void) {
+    efficient();
+ 
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+ 
+    return 0;
+    // Every problem has its weakness.
+    // This problem's weakness is...the existence of me!
+}
